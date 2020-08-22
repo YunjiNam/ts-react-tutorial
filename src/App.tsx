e032c57a4fbda0f61328c6cb91e1c08224e6b9f3
yunjiNam <yj607252@gmail.com>
2020-08-22 21:23:48 +0900

    study: basic typescript3 study

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import Greetings from "./Greetings";
+import ReducerSample from "./ReducerSample";
+//import Greetings from "./Greetings";
+//import Counter from "./Counter";
+//import MyForm from "./MyForm";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="Hello" onClick={onClick} />;
+  // const onClick = (name: string) => {
+  //   console.log(`${name} says hello`);
+  // };
+  //return <Greetings name="Hello" onClick={onClick} />;
+  //return <Counter />;
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
+  // return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 };
 
 export default App;
